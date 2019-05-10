573121bc851f94d53aaf85b12924da8b70b7858b
yeah-right <claar.zack@gmail.com>
2019-05-09 18:57:44 -0600

    CRUD actions working locally, pushing to master then pushing to heroku

--- app/controllers/api/v1/habits_controller.rb
+++ app/controllers/api/v1/habits_controller.rb
@@ -6,21 +6,45 @@ class Api::V1::HabitsController < ApplicationController
         render json: @habits
     end 
 
+    def show 
+      find_habit()
+    end 
+
     def new 
       @habit = Habit.new
     end 
 
     def create
+      @habit = Habit.new(habit_params)
+      if @habit.save 
+        render json: @habit
+      else  
+        render json: { errors: @habit.errors.full_messages }, status: :unprocessible_entity 
+      end 
     end
 
+    def edit 
+      @habit = Habit.find(params[:id])
+    end 
+
+    def update 
+      @habit = Habit.find(params[:id])
+      @habit.update(habit_params)
+    end 
+
+    def destroy
+      @habit = Habit.find(params[:id])
+      @habit.destroy
+      render json: {status: :ok}, status: :ok
+    end
 
     private 
 
     def habit_params
-        params.permit(:title, :content)
+        params.permit(:name, :user_id)
       end
      
-      def find_habit
-        @habit = Habit.find(params[:id])
-      end
+      # def find_habit
+      #   @habit = Habit.find(params[:id])
+      # end
 end
